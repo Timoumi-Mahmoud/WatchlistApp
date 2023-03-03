1ef3ac02225b8b556c56d35d83b53a92e21bf6f5
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-03-03 22:31:45 +0200

    t

--- src/main/java/com/example/watchlist/WatchlistApplication.java
+++ src/main/java/com/example/watchlist/WatchlistApplication.java
@@ -2,9 +2,12 @@ package com.example.watchlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WatchlistApplication {
+public class WatchlistApplication   {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WatchlistApplication.class, args);
